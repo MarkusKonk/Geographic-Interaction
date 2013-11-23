ff8b4986f8c2dd6d9a679725c322ffc767f61d87
Markus <mkonkol@arcor.de>
2013-11-23 17:49:34 +0100

    .php script improvements

--- website/database_select.php
+++ website/database_select.php
@@ -3,15 +3,15 @@ $dbconn = pg_connect("host=giv-geointeraction.uni-muenster.de port=5432 dbname=f
     or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
 	
 	$col_valueList = array();
-	for($i=0;$i<200;$i++){
-	$query=pg_query($dbconn,'select "ID","Point" FROM "Feature" where "ID"='.$i.';');
+
+	$query=pg_query($dbconn,'select "ID","Point" FROM "Feature";');
 	
 	while ($line = pg_fetch_array($query, null, PGSQL_ASSOC)) {
     foreach ($line as $col_value) {
 		//$col_value=substr($col_value,1,$col_value.length-1);
 		array_push($col_valueList,$col_value);
 		}
-    }	
+    
 }
 	$JSON = "["; 
 	for ($i=0;$i<count($col_valueList);$i+=2){
--- website/temp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbconn = pg_connect("host=giv-geointeraction.uni-muenster.de port=5432 dbname=featuredb user=postgres password=IGIpostgres")
-    or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
-
-	$query=pg_query($dbconn,'ALTER TABLE "Feature" ADD Number INTEGER;');
-
-?>
