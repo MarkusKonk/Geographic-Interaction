a935f85297bde06fa0512b7f1e493b43a54112c6
Markus <mkonkol@arcor.de>
2013-12-09 11:50:43 +0100

    Attributes for Lines

--- website/database_addLineAttributes.php
+++ website/database_addLineAttributes.php
@@ -3,16 +3,15 @@ $dbconn = pg_connect("host=giv-geointeraction.uni-muenster.de port=5432 dbname=f
     or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
 	
 	
-	//$Name = $_POST['Name'];
-	$Name = "NAME";
-	//$Description = $_POST['Description'];
-	$Description = "DESCRIPTION";
-	//$Comments = $_POST['Comments'];
-	$LatLng = $_POST['Coordinates'];
-	$Roadtype = $_POST['Roadtype'];
+	$Name = $_POST['Name'];
 	$Description = $_POST['Description'];
+	$ID = $_POST['ID']; 
+	//$Comments = $_POST['Comments'];
+	$type = $_POST['Type'];
 
-	$query=pg_query($dbconn,"Insert into \"Lines\" values (DEFAULT,'".$LatLng."','".$Roadtype."','".$Name."','".$Description."');");
-
+	
+	$query=pg_query($dbconn,"UPDATE \"Lines\" SET \"Name\"='".$Name."' where \"ID\"=".$ID.";");
+	$query=pg_query($dbconn,"UPDATE \"Lines\" SET \"Description\"='".$Description."' where \"ID\"=".$ID.";");
+	$query=pg_query($dbconn,"UPDATE \"Lines\" SET \"IsMainRoad\"='".$type."' where \"ID\"=".$ID.";");
 	
 ?>
--- website/database_insertLines.php
+++ website/database_insertLines.php
@@ -9,8 +9,8 @@ $dbconn = pg_connect("host=giv-geointeraction.uni-muenster.de port=5432 dbname=f
 	$Description = "DESCRIPTION";
 	//$Comments = $_POST['Comments'];
 	$LatLng = $_POST['Coordinates'];
-	$Roadtype = $_POST['Roadtype'];
-	$Description = $_POST['Description'];
+	$Roadtype = "false";
+	
 
 	$query=pg_query($dbconn,"Insert into \"Lines\" values (DEFAULT,'".$LatLng."','".$Roadtype."','".$Name."','".$Description."');");
 
